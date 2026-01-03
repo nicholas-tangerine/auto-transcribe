97fa11793d3089d179de81d28f59e483f22d96fa
Nicholas Tang <ntang11@ucsc.edu>
2026-01-03 00:07:16 -0800

    tracks notes instead of freq now, added untested tracking functionality

--- include/pitch-tracker.h
+++ include/pitch-tracker.h
@@ -6,31 +6,42 @@
 
 /**
  * This struct keeps track of frequency domain graph over time. Treated as a
- * circular buffer, with a LRU replacement policy
+ * ring buffer.
  */
 typedef struct {
-    double **note;          //  note[frame][n] -> amplitude 
-    bool *note_active;      //  note_active[frame][n] -> on/off
+    double **note_buffer;   //  note[frame][n] -> amplitude 
+    bool *note_active;      //  note_active[n] -> on/off
     uint64_t frame_count;   //  number of frames to keep histories of; treated as circular buffer
     uint64_t curr;          //  index of oldest frame
+    uint32_t num_notes;     //  number of notes in a frame
 } pitch_tracker_t;
 
 /**
  * Allocates a clean pitch_tracker_t struct
  *
  * @param frame_count       the number of frames to keep in tracker history
- * @param freq_len          the number of frequency bins
- * @param freq_min          the frequency of the 0th frequency bin
- * @param freq_max          the frequency of the last frequency bin
+ * @param num_notes         the number of notes in each frame from (and
+ *                          including) A0
  */
-pitch_tracker_t *pitch_tracker_init(uint64_t frame_count);
+pitch_tracker_t *pitch_tracker_init(uint64_t frame_count, uint32_t num_notes);
 
 /**
- * Add a frequency representation array to the pitch tracker. 
+ * Add a notes representation array to the pitch tracker. 
  *
  * @param pitch_tracker     pitch_tracker_t struct containing previous history
- * @param freq              frequency representation of waveform
+ * @param notes             notes/musical representation of waveform
  */
-void add_freq(pitch_tracker_t *pitch_tracker, double *freq);
+void add_frame(pitch_tracker_t *pitch_tracker, double *notes);
+
+/**
+ * Takes the buffer and marks the notes that are active and not active. For a
+ * note to be considered "active", it must be present in at least k frames.
+ * Output is stored in the note_active array in pitch_tracker struct
+ *
+ * @param pitch_tracker     a struct storing all the frames
+ * @param k                 the number of times a note has to be heard to be
+ *                          considered "active"
+ */
+void extract_true_notes(pitch_tracker_t *pitch_tracker, uint32_t k);
 
 #endif //   PITCH_TRACKER_H_
--- src/pitch-tracker.c
+++ src/pitch-tracker.c
@@ -1,19 +1,39 @@
 #include "pitch-tracker.h"
 
-pitch_tracker_t *pitch_tracker_init(uint64_t frame_count, uint64_t freq_len,
-        uint32_t freq_min, uint32_t freq_max) {
+#include <stdlib.h>
+
+pitch_tracker_t *pitch_tracker_init(uint64_t frame_count, uint32_t num_notes) {
     pitch_tracker_t *out = (pitch_tracker_t *) malloc(sizeof(pitch_tracker_t));
 
-    out->freq = malloc(sizeof(double *) * freq_len);
-    out->note = malloc(/* NUMBER OF NOTES TO KEEP TRACK OF */);
-    out->note_active = malloc(sizeof(bool) * freq_len);
+    out->note_buffer = malloc(sizeof(double *) * frame_count);
+    out->note_active = malloc(sizeof(bool) * num_notes);
     out->frame_count = frame_count;
     out->curr = 0;
+    out->num_notes = num_notes;
 
     return out;
 }
 
-void add_frame(pitch_tracker_t *pitch_tracker, double *freq) {
-    pitch_tracker->freq[pitch_tracker->curr] = freq;
+void add_frame(pitch_tracker_t *pitch_tracker, double *notes) {
+    pitch_tracker->note_buffer[pitch_tracker->curr] = notes;
     pitch_tracker->curr = (pitch_tracker->curr + 1) % pitch_tracker->frame_count;
+}
+
+void extract_true_notes(pitch_tracker_t *pitch_tracker, uint32_t k) {
+    double **buffer = pitch_tracker->note_buffer;
+    uint32_t num_notes = pitch_tracker->num_notes;
+
+    //  MARK: probably lots of cache misses here
+    for (uint32_t n = 0; n < num_notes; n++) {
+        uint32_t counter = 0;
+        for (uint64_t i = 0; i < pitch_tracker->frame_count; i++) {
+            uint64_t curr_index = (pitch_tracker->curr + i) % pitch_tracker->frame_count;
+            double *notes_arr = buffer[curr_index];
+
+            if (notes_arr[n] > 0.0) counter++;
+        } // frame in frames
+
+        pitch_tracker->note_active[n] = counter >= k ? true : false;
+    } // note in notes
+    return;
 }
